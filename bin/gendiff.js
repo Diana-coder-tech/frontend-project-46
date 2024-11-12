2d8d85955c6446816901e0f173ba5cba26e9cd6d
Diana Zhiliakova <zhiliackova2009@yandex.ru>
2024-11-12 10:35:29 +0300

    change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,13 @@ program
   .name('description')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0') 
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    // Временный вывод аргументов и опций для проверки работы cli-утилиты
+    console.log('Filepath 1:', filepath1);
+    console.log('Filepath 2:', filepath2);
+    console.log('Format:', options.format);
+  });
   
 program.parse();
