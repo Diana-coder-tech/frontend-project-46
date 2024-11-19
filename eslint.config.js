229412b90f46fedbe91b6330496757793904617d
Diana Zhiliakova <zhiliackova2009@yandex.ru>
2024-11-19 17:23:30 +0300

    change files

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,45 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import path from 'path';
 import { FlatCompat } from '@eslint/eslintrc';
 import { fileURLToPath } from 'url';
 
-// mimic CommonJS variables -- not needed if using CommonJS
+// Определение __filename и __dirname для ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Совместимость с CommonJS конфигурациями
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
+  baseDirectory: __dirname, // Указание базовой директории
 });
+
+// Экспорт конфигурации
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      parserOptions: {
-        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
-        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+  ...compat.config({
+    parserOptions: {
+      ecmaVersion: 'latest', // Современная версия ECMAScript
+      sourceType: 'module', // Использование ES-модулей
     },
-
-  },
- 
-  {
+    env: {
+      node: true, // Среда выполнения Node.js
+      jest: true, // Поддержка глобальных переменных Jest
+    },
+    globals: {
+      ...globals.node, // Глобальные переменные Node.js
+      ...globals.jest, // Глобальные переменные Jest
+    },
     rules: {
       'no-underscore-dangle': [
         'error',
         {
-          allow: ['__filename', '__dirname'],
+          allow: ['__filename', '__dirname'], // Разрешение использования __filename и __dirname
         },
       ],
-      
-      'import/no-named-as-default': 'off',
+      'import/no-named-as-default': 'off', // Отключение определённых импортных правил
       'import/no-named-as-default-member': 'off',
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
     },
-  },
+  }),
 ];
